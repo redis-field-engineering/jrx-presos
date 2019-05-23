a1da4a7a153ab15fed3ed2f69c01325b48f19e35
Julien Ruaux <jruaux@gmail.com>
2019-05-22 21:09:21 -0700

    fixed background for images

--- render_all.sh
+++ render_all.sh
@@ -1,4 +1,4 @@
 cd docs
-bundle exec asciidoctor-revealjs -a revealjs_history=true -a revealjs_theme=blood -a revealjs_slideNumber=true -a linkcss -a customcss=style.css -a revealjsdir=reveal.js-3.8.0 -r asciidoctor-diagram *.adoc
+bundle exec asciidoctor-revealjs -a revealjs_history=true -a revealjs_theme=league -a revealjs_slideNumber=true -a linkcss -a customcss=style.css -a revealjsdir=reveal.js-3.8.0 -r asciidoctor-diagram *.adoc
 asciidoctor index.adoc
 cd ..
