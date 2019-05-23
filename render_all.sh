1865e5756f3dcd6121b3a793100e03b9a4115b97
Julien Ruaux <jruaux@gmail.com>
2019-05-22 20:57:32 -0700

    fixed index

--- render_all.sh
+++ render_all.sh
@@ -1,2 +1,4 @@
-bundle exec asciidoctor-revealjs -a revealjs_history=true -a revealjs_theme=blood -a revealjs_slideNumber=true -a linkcss -a customcss=style.css -a revealjsdir=./reveal.js-3.8.0 -r asciidoctor-diagram docs/*.adoc
-asciidoctor docs/index.adoc
+cd docs
+bundle exec asciidoctor-revealjs -a revealjs_history=true -a revealjs_theme=blood -a revealjs_slideNumber=true -a linkcss -a customcss=style.css -a revealjsdir=reveal.js-3.8.0 -r asciidoctor-diagram *.adoc
+asciidoctor index.adoc
+cd ..
