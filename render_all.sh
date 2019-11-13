dc33888ec35e3028607c65dd4e2455eae6120e98
Julien Ruaux <jruaux@gmail.com>
2019-11-13 14:38:36 -0800

    updated docs

--- render_all.sh
+++
@@ -1,4 +0,0 @@
-cd docs
-bundle exec asciidoctor-revealjs -a revealjs_history=true -a revealjs_theme=league -a revealjs_slideNumber=true -a linkcss -a customcss=style.css -a revealjsdir=reveal.js-3.8.0 -r asciidoctor-diagram *.adoc
-asciidoctor index.adoc
-cd ..
